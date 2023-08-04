1ed2dfe54fc2230d274c6229723ce4580fa581b4
Samarth-kiwi <128691674+Samarth-kiwi@users.noreply.github.com>
2023-08-05 08:22:50 +1000

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC4sUJo5TVDlgilmFe7gMvgJzuaAl0zcXQ",
+    authDomain: "lets-chat-4b70e.firebaseapp.com",
+    projectId: "lets-chat-4b70e",
+    storageBucket: "lets-chat-4b70e.appspot.com",
+    messagingSenderId: "718444450632",
+    appId: "1:718444450632:web:6614b90d429cba2e40197d"
+  };
